0c4494491208a8065b6845a8a8e93cd261fa08bc
MJ Brechner <mjbrechner@gmail.com>
2025-11-09 14:44:38 -0500

    added physical traits to be copied, prevents copying duplicate sets

--- javascript/copier.js
+++ javascript/copier.js
@@ -2,6 +2,7 @@
 
 const copiedSection = document.getElementById("copied-characters");
 let NumberOfCopies = 0;
+let copiedData = [];
 
 function copier() {
     NumberOfCopies++;
@@ -17,13 +18,13 @@ function copier() {
     let newCharacterAreaDetail = document.createElement("div");
     newCharacterAreaDetail.setAttribute("class", "character-area-detail");
 
+    // personality copier start
     let newCharacterAreaPersonality = document.createElement("div");
     newCharacterAreaPersonality.setAttribute("class", "character-section");
     newCharacterAreaPersonality.setAttribute("class", "personality-section");
 
-
-    let newCharacterAreaDetailHeading = document.createElement("h3");
-    newCharacterAreaDetailHeading.textContent = "personality";
+    let newCharacterAreaDetailPersonalityHeading = document.createElement("h3");
+    newCharacterAreaDetailPersonalityHeading.textContent = "personality";
 
     let newCharacterAreaPersonalityList = document.createElement("ul");
 
@@ -35,19 +36,62 @@ function copier() {
     newCharacterAreaPersonalityListElement2.setAttribute("class", "trait");
     newCharacterAreaPersonalityListElement2.textContent = document.getElementById("personality-trait-2").innerText;
 
-
     let newCharacterAreaPersonalityListElement3 = document.createElement("li");
     newCharacterAreaPersonalityListElement3.setAttribute("class", "trait");
     newCharacterAreaPersonalityListElement3.textContent = document.getElementById("personality-trait-3").innerText;
+    // personality copier end
 
+    // physical copier start
+    let newCharacterAreaPhysical = document.createElement("div");
+    newCharacterAreaPhysical.setAttribute("class", "character-section");
+    newCharacterAreaPhysical.setAttribute("class", "physical-section");
 
+    let newCharacterAreaDetailPhysicalHeading = document.createElement("h3");
+    newCharacterAreaDetailPhysicalHeading.textContent = "physical";
+
+    let newCharacterAreaPhysicalList = document.createElement("ul");
+
+    let newCharacterAreaPhysicalListElement1 = document.createElement("li");
+    newCharacterAreaPhysicalListElement1.setAttribute("class", "trait");
+    newCharacterAreaPhysicalListElement1.textContent = document.getElementById("physical-trait-1").innerText;
+
+    let newCharacterAreaPhysicalListElement2 = document.createElement("li");
+    newCharacterAreaPhysicalListElement2.setAttribute("class", "trait");
+    newCharacterAreaPhysicalListElement2.textContent = document.getElementById("physical-trait-2").innerText;
+
+    let newCharacterAreaPhysicalListElement3 = document.createElement("li");
+    newCharacterAreaPhysicalListElement3.setAttribute("class", "trait");
+    newCharacterAreaPhysicalListElement3.textContent = document.getElementById("physical-trait-3").innerText;
+    // physical copier end
+
+    let copiedDataNew = [document.getElementById("personality-trait-1").innerText, document.getElementById("personality-trait-2").innerText, document.getElementById("personality-trait-3").innerText, newCharacterAreaPhysicalListElement1.textContent = document.getElementById("physical-trait-1").innerText, newCharacterAreaPhysicalListElement1.textContent = document.getElementById("physical-trait-2").innerText, newCharacterAreaPhysicalListElement1.textContent = document.getElementById("physical-trait-3").innerText];
+    console.log("copiedData: " + copiedData);
+    console.log("copiedDataNew: " + copiedDataNew);
+
+    if (JSON.stringify(copiedDataNew) === JSON.stringify(copiedData)) {
+        console.log("failed attempt to copy duplicate");
+        alert("already copied this trait box");
+        return // If the newly copied data is simply an exact copy of the previously copied data, just exist the function without creating a new duplicate box.
+    } else {
+        console.log("will now make copy");
+        copiedData = copiedDataNew; // The new data will now be registered so it can't be duplicated next time.
+    }
+
+    //build the copy
     copiedSection.appendChild(newCharacterArea);
     newCharacterArea.appendChild(newHeading);
     newCharacterArea.appendChild(newCharacterAreaDetail);
     newCharacterAreaDetail.appendChild(newCharacterAreaPersonality);
-    newCharacterAreaPersonality.appendChild(newCharacterAreaDetailHeading);
+    newCharacterAreaPersonality.appendChild(newCharacterAreaDetailPersonalityHeading);
     newCharacterAreaPersonality.appendChild(newCharacterAreaPersonalityList);
     newCharacterAreaPersonalityList.appendChild(newCharacterAreaPersonalityListElement1);
     newCharacterAreaPersonalityList.appendChild(newCharacterAreaPersonalityListElement2);
     newCharacterAreaPersonalityList.appendChild(newCharacterAreaPersonalityListElement3);
+
+    newCharacterAreaDetail.appendChild(newCharacterAreaPhysical);
+    newCharacterAreaPhysical.appendChild(newCharacterAreaDetailPhysicalHeading);
+    newCharacterAreaPhysical.appendChild(newCharacterAreaPhysicalList);
+    newCharacterAreaPhysicalList.appendChild(newCharacterAreaPhysicalListElement1);
+    newCharacterAreaPhysicalList.appendChild(newCharacterAreaPhysicalListElement2);
+    newCharacterAreaPhysicalList.appendChild(newCharacterAreaPhysicalListElement3);
 }
